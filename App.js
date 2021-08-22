842312ff38d89598155468bf594c2492fa7ab622
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-22 16:26:58 +0100

    starter setup

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { StyleSheet, Text, View, ImageBackground } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}>
+        <ImageBackground
+          source={require("./assets/images/ModelS.jpeg")}
+          style={styles.image}
+        />
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,000</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#5c5e62",
+  },
+})
