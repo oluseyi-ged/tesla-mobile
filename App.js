d6f0dc3ae2e9a703388407dedb84e9af382ee53c
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-23 02:14:36 +0100

    flatlist component added for easy scroll of list

--- App.js
+++ App.js
@@ -2,45 +2,12 @@ import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { StyleSheet, Text, View, ImageBackground } from "react-native"
 import CarItem from "./components/CarItem"
+import CarsList from "./components/CarsList"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem
-        name="Model S"
-        tagLine="Order Online for"
-        tagLineCTA="Touchless Delivery"
-        image={require("./assets/images/ModelS.jpeg")}
-      />
-      <CarItem
-        name="Model Y"
-        tagLine="Order Online for"
-        tagLineCTA="Touchless Delivery"
-        image={require("./assets/images/ModelY.jpeg")}
-      />
-      <CarItem
-        name="Model 3"
-        tagLine="Order Online for"
-        image={require("./assets/images/Model3.jpeg")}
-      />
-      <CarItem
-        name="Model X"
-        tagLine="Order Online for"
-        tagLineCTA="Touchless Delivery"
-        image={require("./assets/images/ModelX.jpeg")}
-      />
-      <CarItem
-        name="Solar Panels"
-        tagLine="Order Online for"
-        tagLineCTA="Touchless Delivery"
-        image={require("./assets/images/SolarPanels.jpeg")}
-      />
-      <CarItem
-        name="Solar Roof"
-        tagLine="Order Online for"
-        tagLineCTA="Touchless Delivery"
-        image={require("./assets/images/SolarRoof.jpeg")}
-      />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   )
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -3,14 +3,15 @@ import { View, Text, ImageBackground } from "react-native"
 import styles from "./styles"
 import StyledButton from "../StyledButton"
 
-const CarItem = ({ name, tagLine, image, tagLineCTA }) => {
+const CarItem = (props) => {
+  const { name, tagline, image, taglineCTA } = props.car
   return (
     <View style={styles.carContainer}>
       <ImageBackground source={image} style={styles.image} />
       <View style={styles.titles}>
         <Text style={styles.title}>{name}</Text>
         <Text style={styles.subtitle}>
-          {tagLine} <Text style={styles.subtitleCTA}>{tagLineCTA}</Text>
+          {tagline} <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
         </Text>
       </View>
 
